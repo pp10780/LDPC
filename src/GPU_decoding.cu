cca07d59edb18eed9e137d92154b125ef832d647
zezezedididi <jose.eduardo.diniz@tecnico.ulisboa.pt>
2024-05-14 14:30:11 +0100

    missing includes

--- src/GPU_decoding.cu
+++ src/GPU_decoding.cu
@@ -1,3 +1,15 @@
+#include <stdio.h>
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+#include <driver_functions.h>
+
+#include <thrust/scan.h>
+#include <thrust/device_ptr.h>
+#include <thrust/device_malloc.h>
+#include <thrust/device_free.h>
+
 #include "decoding.h"
 
 //kernel 0: innit -> compute r and Li from m
