3bcdcf44786ffa840e4d97d5cd886abbd54f97d5
zezezedididi <jose.eduardo.diniz@tecnico.ulisboa.pt>
2025-02-03 18:17:20 +0000

    make file working (path needs to be added in the makefile) arquitecture flag needs research

--- Makefile
+++ Makefile
@@ -2,129 +2,119 @@ SRCDIR = src
 OBJDIR = obj
 BINDIR = bin
 MATDIR = matrices
-CUDAPATH = 
+CUDAPATH = /usr/local/cuda-12.4
 
 #remove GPU whilst testing
 #LDPC
-OBJS    = $(addprefix $(OBJDIR)/, main.o decoding.o encoding.o display_variables.o storage.o sparse_decoding.o )
-SOURCE  = $(addprefix $(SRCDIR)/, main.c decoding.c encoding.c display_variables.c storage.c sparse_decoding.c )
-HEADER  = $(addprefix $(SRCDIR)/, decoding.h encoding.h defs.h display_variables.h storage.h sparse_decoding.h ) 
+OBJS    = $(addprefix $(OBJDIR)/, main.o decoding.o encoding.o display_variables.o storage.o sparse_decoding.o GPU_sparse_decoding.o)
+SOURCE  = $(addprefix $(SRCDIR)/, main.cu decoding.cu encoding.cu display_variables.cu storage.cu sparse_decoding.c GPU_sparse_decoding.cu)
+HEADER  = $(addprefix $(SRCDIR)/, decoding.h encoding.h defs.h display_variables.h storage.h sparse_decoding.h GPU_sparse_decoding.h) 
 OUT     = $(BINDIR)/ldpc
 
-#GPU_decoding.cu GPU_decoding.h
-
 CC      = gcc
-FLAGS	= -std=c99 -g -c -Wall
-NVCC 	= nvcc
-CUFLAGS	= -O3 -m64 --gpu-architecture compute_61
+FLAGS	= -std=c99 -g -c -Wall 
+NVCC 	= $(CUDAPATH)/bin/nvcc
+CUFLAGS	= --gpu-architecture=compute_61 -O3 -c
 MATH    = -lm
-CUCCFLAGS = $(CUDAPATH) -lcuda
+LFLAGS = -L/$(CUDAPATH)/lib64 -lcudart
 
 #COMPILING RULES
 $(OBJDIR)/%.o: $(SRCDIR)/%.c $(HEADER) 
 	$(CC) $(FLAGS) $< -o $@
 
-$(OBJDIR)/%.o: $(SRCDIR)/%.cu $(HEADER)
+$(OBJDIR)/%.o: $(SRCDIR)/%.cu $(HEADER) 
 	$(NVCC) $(CUFLAGS) $< -o $@
 
 all: $(OUT)
 
 ldpc: $(OUT)
 $(OUT): $(OBJS)
-	$(CC) -g $(OBJS) -o $(OUT) $(LFLAGS) $(MATH) $(CUCCFLAGS)
+	$(CC) -g $(OBJS) -o $(OUT) $(LFLAGS) $(MATH) 
 
-gpu: 
-	nvcc -O3 -m64 --gpu-architecture compute_61 src/GPU_sparse_decoding.cu -o bin/GPU_sparse
 test: 
-	./bin/GPU_sparse matrices/G1.csr matrices/H1.csr 0.1
+	$(OUT) matrices/G1.csr matrices/H1.csr 0.1
 
 tests: 
 	@echo =====1000=====
 	@echo rate 0.2
-#	./bin/GPU_sparse matrices/G1 tests/tests_1000_0.2_1
-	./bin/GPU_sparse tests/tests_1000_0.2_3G tests/tests_1000_0.2_3
-	./bin/GPU_sparse tests/tests_1000_0.2_5G tests/tests_1000_0.2_5
-	./bin/GPU_sparse tests/tests_1000_0.2_9G tests/tests_1000_0.2_9
+#	$(OUT) matrices/G1 tests/tests_1000_0.2_1
+	$(OUT) tests/tests_1000_0.2_3G tests/tests_1000_0.2_3
+	$(OUT) tests/tests_1000_0.2_5G tests/tests_1000_0.2_5
+	$(OUT) tests/tests_1000_0.2_9G tests/tests_1000_0.2_9
 	@echo rate 0.1
-#	./bin/GPU_sparse matrices/G1 tests/tests_1000_0.1_1
-	./bin/GPU_sparse tests/tests_1000_0.1_3G tests/tests_1000_0.1_3
-	./bin/GPU_sparse tests/tests_1000_0.1_5G tests/tests_1000_0.1_5
-	./bin/GPU_sparse tests/tests_1000_0.1_9G tests/tests_1000_0.1_9
+#	$(OUT) matrices/G1 tests/tests_1000_0.1_1
+	$(OUT) tests/tests_1000_0.1_3G tests/tests_1000_0.1_3
+	$(OUT) tests/tests_1000_0.1_5G tests/tests_1000_0.1_5
+	$(OUT) tests/tests_1000_0.1_9G tests/tests_1000_0.1_9
 	@echo rate 0.05
-#	./bin/GPU_sparse matrices/G1 tests/tests_1000_0.05_1
-	./bin/GPU_sparse tests/tests_1000_0.05_3G tests/tests_1000_0.05_3
-	./bin/GPU_sparse tests/tests_1000_0.05_5G tests/tests_1000_0.05_5
-	./bin/GPU_sparse tests/tests_1000_0.05_9G tests/tests_1000_0.05_9
+#	$(OUT) matrices/G1 tests/tests_1000_0.05_1
+	$(OUT) tests/tests_1000_0.05_3G tests/tests_1000_0.05_3
+	$(OUT) tests/tests_1000_0.05_5G tests/tests_1000_0.05_5
+	$(OUT) tests/tests_1000_0.05_9G tests/tests_1000_0.05_9
 	@echo rate 0.02
-#	./bin/GPU_sparse matrices/G1 tests/tests_1000_0.02_1
-	./bin/GPU_sparse tests/tests_1000_0.02_3G tests/tests_1000_0.02_3
-	./bin/GPU_sparse tests/tests_1000_0.02_5G tests/tests_1000_0.02_5
-	./bin/GPU_sparse tests/tests_1000_0.02_9G tests/tests_1000_0.02_9
+#	$(OUT) matrices/G1 tests/tests_1000_0.02_1
+	$(OUT) tests/tests_1000_0.02_3G tests/tests_1000_0.02_3
+	$(OUT) tests/tests_1000_0.02_5G tests/tests_1000_0.02_5
+	$(OUT) tests/tests_1000_0.02_9G tests/tests_1000_0.02_9
 	@echo rate 0.01
-#	./bin/GPU_sparse matrices/G1 tests/tests_1000_0.01_1
-	./bin/GPU_sparse tests/tests_1000_0.01_3G tests/tests_1000_0.01_3
-	./bin/GPU_sparse tests/tests_1000_0.01_5G tests/tests_1000_0.01_5
-	./bin/GPU_sparse tests/tests_1000_0.01_9G tests/tests_1000_0.01_9
+#	$(OUT) matrices/G1 tests/tests_1000_0.01_1
+	$(OUT) tests/tests_1000_0.01_3G tests/tests_1000_0.01_3
+	$(OUT) tests/tests_1000_0.01_5G tests/tests_1000_0.01_5
+	$(OUT) tests/tests_1000_0.01_9G tests/tests_1000_0.01_9
 
 break:
 	@echo =====10000=====
 	@echo rate 0.2
-#	./bin/GPU_sparse matrices/G1 tests/tests_10000_0.2_1
-	./bin/GPU_sparse tests/tests_10000_0.2_3G tests/tests_10000_0.2_3
-	./bin/GPU_sparse tests/tests_10000_0.2_5G tests/tests_10000_0.2_5
-	./bin/GPU_sparse tests/tests_10000_0.2_9G tests/tests_10000_0.2_9
+#	$(OUT) matrices/G1 tests/tests_10000_0.2_1
+	$(OUT) tests/tests_10000_0.2_3G tests/tests_10000_0.2_3
+	$(OUT) tests/tests_10000_0.2_5G tests/tests_10000_0.2_5
+	$(OUT) tests/tests_10000_0.2_9G tests/tests_10000_0.2_9
 	@echo rate 0.1
-#	./bin/GPU_sparse matrices/G1 tests/tests_10000_0.1_1
-	./bin/GPU_sparse tests/tests_10000_0.1_3G tests/tests_10000_0.1_3
-	./bin/GPU_sparse tests/tests_10000_0.1_5G tests/tests_10000_0.1_5
-	./bin/GPU_sparse tests/tests_10000_0.1_9G tests/tests_10000_0.1_9
+#	$(OUT) matrices/G1 tests/tests_10000_0.1_1
+	$(OUT) tests/tests_10000_0.1_3G tests/tests_10000_0.1_3
+	$(OUT) tests/tests_10000_0.1_5G tests/tests_10000_0.1_5
+	$(OUT) tests/tests_10000_0.1_9G tests/tests_10000_0.1_9
 	@echo rate 0.05
-#	./bin/GPU_sparse matrices/G1 tests/tests_10000_0.05_1
-	./bin/GPU_sparse tests/tests_10000_0.05_3G tests/tests_10000_0.05_3
-	./bin/GPU_sparse tests/tests_10000_0.05_5G tests/tests_10000_0.05_5
-	./bin/GPU_sparse tests/tests_10000_0.05_9G tests/tests_10000_0.05_9
+#	$(OUT) matrices/G1 tests/tests_10000_0.05_1
+	$(OUT) tests/tests_10000_0.05_3G tests/tests_10000_0.05_3
+	$(OUT) tests/tests_10000_0.05_5G tests/tests_10000_0.05_5
+	$(OUT) tests/tests_10000_0.05_9G tests/tests_10000_0.05_9
 	@echo rate 0.02
-#	./bin/GPU_sparse matrices/G1 tests/tests_10000_0.02_1
-	./bin/GPU_sparse tests/tests_10000_0.02_3G tests/tests_10000_0.02_3
-	./bin/GPU_sparse tests/tests_10000_0.02_5G tests/tests_10000_0.02_5
-	./bin/GPU_sparse tests/tests_10000_0.02_9G tests/tests_10000_0.02_9
+#	$(OUT) matrices/G1 tests/tests_10000_0.02_1
+	$(OUT) tests/tests_10000_0.02_3G tests/tests_10000_0.02_3
+	$(OUT) tests/tests_10000_0.02_5G tests/tests_10000_0.02_5
+	$(OUT) tests/tests_10000_0.02_9G tests/tests_10000_0.02_9
 	@echo rate 0.01
-#	./bin/GPU_sparse matrices/G1 tests/tests_10000_0.01_1
-	./bin/GPU_sparse tests/tests_10000_0.01_3G tests/tests_10000_0.01_3
-	./bin/GPU_sparse tests/tests_10000_0.01_5G tests/tests_10000_0.01_5
-	./bin/GPU_sparse tests/tests_10000_0.01_9G tests/tests_10000_0.01_9
+#	$(OUT) matrices/G1 tests/tests_10000_0.01_1
+	$(OUT) tests/tests_10000_0.01_3G tests/tests_10000_0.01_3
+	$(OUT) tests/tests_10000_0.01_5G tests/tests_10000_0.01_5
+	$(OUT) tests/tests_10000_0.01_9G tests/tests_10000_0.01_9
 	@echo =====100000=====
 	@echo rate 0.2
-#	./bin/GPU_sparse matrices/G1 tests/tests_100000_0.2_1
-	./bin/GPU_sparse matrices/G1 tests/tests_100000_0.2_3
-	./bin/GPU_sparse matrices/G1 tests/tests_100000_0.2_5
-	./bin/GPU_sparse matrices/G1 tests/tests_100000_0.2_9
+#	$(OUT) matrices/G1 tests/tests_100000_0.2_1
+	$(OUT) matrices/G1 tests/tests_100000_0.2_3
+	$(OUT) matrices/G1 tests/tests_100000_0.2_5
+	$(OUT) matrices/G1 tests/tests_100000_0.2_9
 	@echo rate 0.1
-#	./bin/GPU_sparse matrices/G1 tests/tests_100000_0.1_1
-	./bin/GPU_sparse matrices/G1 tests/tests_100000_0.1_3
-	./bin/GPU_sparse matrices/G1 tests/tests_100000_0.1_5
-	./bin/GPU_sparse matrices/G1 tests/tests_100000_0.1_9
+#	$(OUT) matrices/G1 tests/tests_100000_0.1_1
+	$(OUT) matrices/G1 tests/tests_100000_0.1_3
+	$(OUT) matrices/G1 tests/tests_100000_0.1_5
+	$(OUT) matrices/G1 tests/tests_100000_0.1_9
 	@echo rate 0.05
-#	./bin/GPU_sparse matrices/G1 tests/tests_100000_0.05_1
-	./bin/GPU_sparse matrices/G1 tests/tests_100000_0.05_3
-	./bin/GPU_sparse matrices/G1 tests/tests_100000_0.05_5
-	./bin/GPU_sparse matrices/G1 tests/tests_100000_0.05_9
+#	$(OUT) matrices/G1 tests/tests_100000_0.05_1
+	$(OUT) matrices/G1 tests/tests_100000_0.05_3
+	$(OUT) matrices/G1 tests/tests_100000_0.05_5
+	$(OUT) matrices/G1 tests/tests_100000_0.05_9
 	@echo rate 0.02
-#	./bin/GPU_sparse matrices/G1 tests/tests_100000_0.02_1
-	./bin/GPU_sparse matrices/G1 tests/tests_100000_0.02_3
-	./bin/GPU_sparse matrices/G1 tests/tests_100000_0.02_5
-	./bin/GPU_sparse matrices/G1 tests/tests_100000_0.02_9
+#	$(OUT) matrices/G1 tests/tests_100000_0.02_1
+	$(OUT) matrices/G1 tests/tests_100000_0.02_3
+	$(OUT) matrices/G1 tests/tests_100000_0.02_5
+	$(OUT) matrices/G1 tests/tests_100000_0.02_9
 	@echo rate 0.01
-#	./bin/GPU_sparse matrices/G1 tests/tests_100000_0.01_1
-	./bin/GPU_sparse matrices/G1 tests/tests_100000_0.01_3
-	./bin/GPU_sparse matrices/G1 tests/tests_100000_0.01_5
-	./bin/GPU_sparse matrices/G1 tests/tests_100000_0.01_9
+#	$(OUT) matrices/G1 tests/tests_100000_0.01_1
+	$(OUT) matrices/G1 tests/tests_100000_0.01_3
+	$(OUT) matrices/G1 tests/tests_100000_0.01_5
+	$(OUT) matrices/G1 tests/tests_100000_0.01_9
 clean:
 	rm -f $(OBJDIR)/*.o
 	rm -f $(BINDIR)/*
-
-gpu_dense:
-	nvcc -O3 -m64 --gpu-architecture compute_61 src/GPU_decoding.cu -o bin/GPU_dense
-
-gpu_sparse:
-	nvcc -O3 -m64 --gpu-architecture compute_61 src/GPU_sparse_decoding.cu -o bin/GPU_sparse
--- src/GPU_sparse_decoding.cu
+++ src/GPU_sparse_decoding.cu
@@ -4,8 +4,11 @@
 #include "GPU_decoding.h"
 #include "defs.h"
 
+//CL_NV_DEVICE_WARP_SIZE (not working for some reason)
+//TODO: fix CL_NV_DEVICE_WARP_SIZE not working
+const int THREADS_PER_BLOCK=16;
+
 //kernel 0: innit -> compute r and Li from m
-//this is the same as cpu implementation
 __global__ void GPU_sparse_apriori_probabilities(int n_col, float llr_i , int *m, float *r, float *L){
     //llr_i corresponds to the initial llr that's attributed depending on the channel (-llr_i if == 1) 
     int index = blockIdx.x * blockDim.x + threadIdx.x;
@@ -20,64 +23,6 @@ __global__ void GPU_sparse_apriori_probabilities(int n_col, float llr_i , int *m
     L[index] = r_val;
 }
 
-//kernel 1: row wise -> compute M and "LE" from L and E, then compute E from M and "LE"
-__global__ void GPU_sparse_row_wise(int n_row, int n_col, int *H, int *Hi, float *M, float* E, float *L, int *z, int *d_check){
-
-    float LE = 1; //row value used to compute E
-    int j = blockIdx.x * blockDim.x + threadIdx.x; //the thread's assigned row
-    int check=0;
-    float M_val,p;
-    
-    if(j > n_row)
-        return;
-
-    //do full row for M [first recursion]
-    for (int i=Hi[j];i<Hi[j+1];i++){
-
-        //early termination check (this is being done in parallel)
-        check ^= z[H[i]];
-
-        M_val = L[H[i]] - E[i];
-
-        //store row value
-        LE *= tanh(M_val/2);
-        //writing result in global memory
-        M[i] = M_val;
-    }
-
-    //do full row for E [second recursion]
-    for (int i=Hi[j];i<Hi[j+1];i++){
-        //exclude corresponding element from row
-        p = LE/(tanh(M[i]/2) );
-        E[i] = log((1+p)/(1-p));
-    }
-
-    //this is probably very bad maybe do a reduction?
-    if(check == 1)
-        *d_check=0;
-}
-
-//kernel 2: column wise -> compute L and z from E and r
-__global__ void GPU_sparse_column_wise(int n_elements, int n_col, int *H, float* E, float* r,float *L, int *z){
-    int i = (blockIdx.x * blockDim.x + threadIdx.x);//the thread's assigned column
-    float L_val;//only write to global memory in the end
-
-    if(i > n_col)
-        return;
-    
-    L_val=r[i];
-    //going column wise means going through the whole matrix H and if the index is the corresponding column then the element is part of the column
-    for(int si=0; si<n_elements ; si++){
-        if(H[si]==i)
-            L_val+=E[si];
-    }
-
-    L[i] = L_val;
-    z[i] = (L_val < 0) ? 1 : 0;;
-}
-
-/*
-//kernel 1 without storing M
 //kernel 1: row wise -> compute M and "LE" from L and E, then compute E from M and "LE"
 __global__ void GPU_sparse_row_wise(int n_row, int n_col, int *H, int *Hi, float* E, float *L, int *z, int *d_check){
 
@@ -96,7 +41,7 @@ __global__ void GPU_sparse_row_wise(int n_row, int n_col, int *H, int *Hi, float
         check ^= z[H[i]];
         
         //store row value
-        LE *= tanh(L[H[i]] - E[i]/2);
+        LE *= tanh((L[H[i]] - E[i])/2);
     }
 
     //do full row for E [second recursion]
@@ -112,13 +57,34 @@ __global__ void GPU_sparse_row_wise(int n_row, int n_col, int *H, int *Hi, float
         *d_check=0;
 }
 
+/*
+//kernel 2: column wise -> compute L and z from E and r
+__global__ void GPU_sparse_column_wise(int n_elements, int n_col, int *H, float* E, float* r,float *L, int *z){
+    int i = (blockIdx.x * blockDim.x + threadIdx.x);//the thread's assigned column
+    float L_val;//only write to global memory in the end
+
+    if(i > n_col)
+        return;
+    
+    L_val=r[i];
+    //going column wise means going through the whole matrix H and if the index is the corresponding column then the element is part of the column
+    for(int si=0; si<n_elements ; si++){
+        if(H[si]==i)
+            L_val+=E[si];
+    }
+
+    L[i] = L_val;
+    z[i] = (L_val < 0) ? 1 : 0;;
+}
+*/
+
 //kernel 2: column wise using shared memory -> compute L and z from E and r
 __global__ void GPU_sparse_column_wise(int n_elements, int n_col, int *H, float* E, float* r,float *L, int *z){
     int i = (blockIdx.x * blockDim.x + threadIdx.x);//the thread's assigned column
     int block_start = blockIdx.x * blockDim.x; //first column of the block (column for thread 0)
 
-    __shared__ float b_L_val[32];
-    float t_L_val[32];
+    __shared__ float b_L_val[THREADS_PER_BLOCK];
+    float t_L_val[THREADS_PER_BLOCK];
 
     //initiate thread memory
     for(int t=0;t<blockDim.x;t++)
@@ -156,10 +122,9 @@ __global__ void GPU_sparse_column_wise(int n_elements, int n_col, int *H, float*
     L[i] = b_L_val[threadIdx.x];
     z[i] = (b_L_val[threadIdx.x] < 0) ? 1 : 0;;
 }
-*/
 
 // Function to decode the message
-void GPU_sparse_decode(pchk H, int *recv_codeword, int *codeword_decoded, float error_rate){
+extern "C" void GPU_sparse_decode(pchk H, int *recv_codeword, int *codeword_decoded, float error_rate){
 #ifdef TIMES
     float time;
     cudaEvent_t start, stop;
@@ -176,9 +141,8 @@ void GPU_sparse_decode(pchk H, int *recv_codeword, int *codeword_decoded, float 
 #endif
     //initialize device memory
     int check;
-    
-    //TODO: fix CL_NV_DEVICE_WARP_SIZE not working
-    int threads_per_block= 1;//CL_NV_DEVICE_WARP_SIZE (not working for some reason)
+
+    int threads_per_block= THREADS_PER_BLOCK;
     int rw_blocks=(H.n_col +threads_per_block -1)/threads_per_block;
     int cw_blocks=(H.n_row +threads_per_block -1)/threads_per_block;
 
@@ -191,12 +155,10 @@ void GPU_sparse_decode(pchk H, int *recv_codeword, int *codeword_decoded, float 
     cudaMalloc((void **)&dHi, (H.n_row+1) * sizeof(int));
 
     //computation matrices
-    float *M,*E;
-    //float *E;
-    cudaMalloc((void **)&M, H.n_elements * sizeof(float));
+    float *E;
     cudaMalloc((void **)&E, H.n_elements * sizeof(float));
     //E needs to be set at 0 at the start
-    //TODO:this is not working!
+    //TODO:this is not working, but E is starting at 0 anyway.
     //cudaMemset(E,0,H.n_elements  * sizeof(int));
 
     //vectors
@@ -288,21 +250,13 @@ void GPU_sparse_decode(pchk H, int *recv_codeword, int *codeword_decoded, float 
     for (try_n = 0; try_n<MAX_ITERATIONS; try_n++){
         //set early termination do occur
         cudaMemset(d_check,1,sizeof(int));
-        //kernel 1:                                        
-        GPU_sparse_row_wise<<<rw_blocks, threads_per_block>>>(H.n_row, H.n_col, dH, dHi, M, E, L, z, d_check);
-        //GPU_sparse_row_wise<<<rw_blocks, threads_per_block>>>(H.n_row, H.n_col, dH, dHi, E, L, z, d_check);
+        //kernel 1:
+        GPU_sparse_row_wise<<<rw_blocks, threads_per_block>>>(H.n_row, H.n_col, dH, dHi, E, L, z, d_check);
 
         cudaDeviceSynchronize();
 
 #ifdef DEBUG
         printf("iteration nº%d\n",try_n);
-        cudaMemcpy(matrix_debug_print,M,H.n_elements*sizeof(float),cudaMemcpyDeviceToHost);
-        //this is a print vector
-        printf("M:[");
-        for(int i=0;i<H.n_elements;i++){
-            printf("%f,",matrix_debug_print[i]);
-        }
-        printf("]\n\n");
 
         cudaMemcpy(matrix_debug_print,E,H.n_elements*sizeof(float),cudaMemcpyDeviceToHost);
         //this is a print vector
@@ -343,212 +297,4 @@ void GPU_sparse_decode(pchk H, int *recv_codeword, int *codeword_decoded, float 
 #endif
 
     return ;
-}
-
-//REMOVE : FOR TESTING PURPOSES ONLY!
-//REMOVE : FOR TESTING PURPOSES ONLY!
-
-int *generate_random_key(int size){
-    int *key=(int *)malloc(size*sizeof(int));
-    int i;
-
-    for(i=0;i<size;i++)
-        key[i] = rand()%2;
-
-    return key;
-}
-
-int* add_error(int *codeword,int codeword_size,float error_rate,int max_errors){
-    int inverse=(1/error_rate),counter=0;
-    int *transmitted_mesage = (int*)malloc(codeword_size * sizeof(int));
-
-    for(int c=0;c<codeword_size;c++){
-        //error
-        if(rand() % inverse == 0 && (counter < max_errors || max_errors== -1) ){
-            transmitted_mesage[c] = !codeword[c];
-            counter++;
-        }
-        else
-            transmitted_mesage[c] = codeword[c];
-    }
-
-    printf("added %d errors\n",counter);        
-    return transmitted_mesage;
-}
-
-void **get_matrix_from_file(pchk *matrix,char *filename){
-    FILE *f = fopen (filename,"r");
-
-    //Open file to read
-    if(f==NULL){
-        printf("couldn't open matrix file %s\n",filename);
-        exit(1);
-    }    
-
-    //matrix info
-    fread(&(matrix->n_row),sizeof(int),1,f);
-    fread(&(matrix->n_col),sizeof(int),1,f);
-    fread(&(matrix->n_elements),sizeof(int),1,f);
-    fread(&(matrix->type),sizeof(int),1,f);
-
-    if(matrix->type ==0){
-        printf("reading dense matrix!\n");
-        exit(1);
-        //normal
-        matrix->A = (int**)malloc(matrix->n_row*sizeof(int*));
-        for(int r=0;r<matrix->n_row;r++){
-            matrix->A[r] = (int*)malloc(matrix->n_col*sizeof(int));
-            fread(matrix->A[r],sizeof(int),matrix->n_col,f);
-        }
-    }
-    else{
-        //sparse
-        matrix->A    = (int**)malloc(2                 *sizeof(int*));
-        matrix->A[0] = (int *)malloc(matrix->n_elements*sizeof(int ));
-        matrix->A[1] = (int *)malloc((matrix->n_row+1 ) *sizeof(int ));
-
-        fread(matrix->A[0],sizeof(int),matrix->n_elements,f);
-        fread(matrix->A[1],sizeof(int),matrix->n_row+1,f);
-    }
-    fclose(f);
-    return NULL;
-}
-
-void dense_free_pchk(pchk mat){
-    for(int i=0;i<mat.n_row;i++)
-            free(mat.A[i]);
-    free(mat.A);
-}
-
-void sparse_free_pchk(pchk mat){
-    free(mat.A[0]);
-    free(mat.A[1]);
-    free(mat.A);
-}
-
-
-void free_pchk(pchk mat){
-    switch(mat.n_elements){
-        case 0://dense
-            dense_free_pchk(mat);
-            break;
-        default://sparse
-            sparse_free_pchk(mat);
-    }
-}
-
-// Function to encode the key
-void encode(int *key, pchk generator, int *message)
-{
-    if(generator.type == 0){
-        //normal
-        for(int row = 0; row < generator.n_row; row++){
-            for(int col = 0; col < generator.n_col; col++){
-                if(generator.A[row][col]==1)
-                    message[row] ^= key[col];
-            }
-        }
-    }
-    else{
-        //sparse
-        for(int r=0;r<generator.n_row;r++){
-            for (int c = generator.A[1][r]; c <  generator.A[1][r+1]; c++)
-                message[r] ^= key[ generator.A[0][c] ];
-        }
-            
-    }
-
-}
-
-int main(int argc, char *argv[]){
-    float error_rate= DEFAULT_ERROR_RATE;
-    int max_errors = DEFAULT_MAX_ERRORS;
-    int g_flag=1;
-    int key_size=0,message_size=0;
-    //check input arguments
-    if(argc<3 || argc>5){
-        printf("Incorrect usage!\n Correct usage is: ./ldpc G_filepath H_filepath [error rate] [max errors]\n");
-        exit(1);
-    }
-    if(argc>3)
-        error_rate=atof(argv[3]);
-    if(argc>4)
-        max_errors=atoi(argv[4]);
-
-    //get parity check matrices from file
-    pchk H,G;
-    get_matrix_from_file(&G,argv[1]);
-    get_matrix_from_file(&H,argv[2]);
-
-    key_size=G.n_col;
-    message_size=G.n_row;
-
-    if(G.n_row != H.n_col){
-           message_size=H.n_col;
-        key_size=H.n_row;
-        g_flag=0;
-        printf("coding and decoding matrices do not match!\nusing a '0's message with size:%d\n",message_size);
-
-    }
-
-    srand(time(NULL));
-    int *key = generate_random_key(key_size);
-    
-    int *codeword_encoded   = (int*)calloc(message_size,sizeof(int));
-    int *codeword_decoded   = (int*)calloc(message_size,sizeof(int));
-    int *transmitted_mesage;
-
-
-    //ENCDODING
-    if(g_flag)
-        encode((int *)key, G, codeword_encoded);
-
-    //TRANSMISSIONs
-    transmitted_mesage = add_error(codeword_encoded,message_size,error_rate,max_errors);
-        
-    //DECODING
-#ifdef TIMES
-    struct timespec clock_begin, clock_end;
-    clock_gettime(CLOCK_REALTIME, &clock_begin);
-#endif
-
-    GPU_sparse_decode(H, transmitted_mesage, codeword_decoded,error_rate);
-
-    
-#ifdef TIMES
-    clock_gettime(CLOCK_REALTIME, &clock_end);
-    long seconds = clock_end.tv_sec - clock_begin.tv_sec;
-    long nanoseconds = clock_end.tv_nsec - clock_begin.tv_nsec;
-    double elapsed = seconds + nanoseconds*1e-9;
-    printf("decoding time: %f\n",elapsed);
-#endif
-
-    if(codeword_decoded == NULL){
-        printf("Not a valid codeword\n");
-        return 0;
-    }
-
-    //check result
-    int correct=1;
-    int c;
-    for(c=0;c<message_size;c++){
-        if(codeword_encoded[c] != codeword_decoded[c]){
-            printf("decoding is incorrect!\n");
-            correct=0;
-            break;
-        }
-    }
-    if(correct)
-        printf("decoding is correct!\n");
-
-    free_pchk(G);
-    free_pchk(H);
-
-    free(key);
-    free(codeword_encoded);
-    //free(codeword_decoded);
-
-    if(correct)
-        return 0;
-    return 0;
 }
